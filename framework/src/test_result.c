b38518da326b0874f08abef70989aa7c4c53451a
Leo-Paul Cottet <lcottet@student.42lyon.fr>
2024-01-27 13:53:09 +0100

    Changed signal KO result color

--- framework/src/test_result.c
+++ framework/src/test_result.c
@@ -6,7 +6,7 @@
 /*   By: lcottet <lcottet@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/27 13:33:35 by lcottet           #+#    #+#             */
-/*   Updated: 2024/01/27 13:44:14 by lcottet          ###   ########.fr       */
+/*   Updated: 2024/01/27 13:52:34 by lcottet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,9 +25,9 @@ int	result(int result, char *funcname, char *testname, t_unit_total *total)
 	else
 		total->ko++;
 	if (result == SIGSEGV)
-		res = "\033[31m[SEGV]\033[0m\n";
+		res = "\033[33m[SEGV]\033[0m\n";
 	else if (result == SIGBUS)
-		res = "\033[31m[BUSE]\033[0m\n";
+		res = "\033[33m[BUSE]\033[0m\n";
 	else if (result != TEST_OK)
 		res = "\033[31m[KO]\033[0m\n";
 	if (ft_printf("%s: %s: %s", funcname, testname, res) == -1)
