b6f98e533380511b6a150625e13d9cf73b6dc8e1
ibertran <128724498+ibtrd@users.noreply.github.com>
2024-01-27 20:10:26 +0100

    Add bonus signales return value

--- framework_bonus/srcs/test_result_bonus.c
+++ framework_bonus/srcs/test_result_bonus.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   test_result_bonus.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lcottet <lcottet@student.42lyon.fr>        +#+  +:+       +#+        */
+/*   By: ibertran <ibertran@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/27 13:33:35 by lcottet           #+#    #+#             */
-/*   Updated: 2024/01/27 18:58:07 by lcottet          ###   ########.fr       */
+/*   Updated: 2024/01/27 20:08:09 by ibertran         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,14 @@ int	result_log(int result, char *funcname, char *testname, t_unit_total *total)
 		res = MSG_SEGV_NOCOLOR;
 	else if (result == SIGBUS)
 		res = MSG_BUSE_NOCOLOR;
+	else if (result == SIGABRT)
+		res = MSG_SIGABRT;
+	else if (result == SIGFPE)
+		res = MSG_SIGFPE;
+	else if (result == SIGPIPE)
+		res = MSG_SIGPIPE;
+	else if (result == SIGILL)
+		res = MSG_SIGILL;
 	else
 		res = MSG_KO_NOCOLOR;
 	if (dprintf(total->logfd, "%s: %s: %s\n", funcname, testname, res) == -1)
