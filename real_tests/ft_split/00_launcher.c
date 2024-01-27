5d95640c908f0ad7c25ae4fc90017252036ffd37
Aleksandr Ermolov <alermolo@z4r3p3.42lyon.fr>
2024-01-27 16:06:21 +0100

    ft_split basic test

--- real_tests/ft_split/00_launcher.c
+++ real_tests/ft_split/00_launcher.c
@@ -1,18 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   real_tests.h                                       :+:      :+:    :+:   */
+/*   00_launcher.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: alermolo <alermolo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/27 15:45:07 by ibertran          #+#    #+#             */
-/*   Updated: 2024/01/27 16:05:21 by alermolo         ###   ########.fr       */
+/*   Created: 2024/01/27 15:55:02 by alermolo          #+#    #+#             */
+/*   Updated: 2024/01/27 16:05:41 by alermolo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef REAL_TESTS_H
-# define REAL_TESTS_H
+#include "libunit.h"
+#include "../real_tests.h"
 
-int	split_basic_test(void);
+int	test_launcher(void)
+{
+	t_test	*test;
 
-#endif
+	test = NULL;
+	add_test(&test, "Basic test", split_basic_test());
+	return (run_tests("test", test));
+}
