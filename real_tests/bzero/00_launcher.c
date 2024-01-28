0f1d9aeffc3a49a952c04fa5d6bb87c41d7c733c
ibertran <128724498+ibtrd@users.noreply.github.com>
2024-01-28 14:10:55 +0100

    Fix cleans / fclean rules

--- real_tests/bzero/00_launcher.c
+++ real_tests/bzero/00_launcher.c
@@ -6,7 +6,7 @@
 /*   By: ibertran <ibertran@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/27 16:05:30 by ibertran          #+#    #+#             */
-/*   Updated: 2024/01/27 17:59:33 by ibertran         ###   ########lyon.fr   */
+/*   Updated: 2024/01/28 14:09:23 by ibertran         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int	bzero_launcher(void)
 	test = NULL;
 	add_test(&test, "basic test", bzero_basic_test);
 	add_test(&test, "size 0", bzero_size_zero);
-	add_test(&test, "null (should SEGV)", bzero_null);
-	add_test(&test, "negative size (should SEGV)", bzero_negative_size);
+	add_test(&test, "null", bzero_null);
+	add_test(&test, "negative size", bzero_negative_size);
 	return (run_tests("BZERO", test));
 }
